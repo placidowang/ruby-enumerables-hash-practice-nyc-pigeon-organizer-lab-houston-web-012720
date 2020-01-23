f22f9ee691185135561ebca9cb8e78997a2ad1c2
Placido Wang <placidow@gmail.com>
2020-01-23 08:46:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -71,6 +71,6 @@ pigeon_data = {
   }
 }
 
-pp nyc_pigeon_organizer(pigeon_data)
-get_pigeon_info(pigeon_data, "Peter Jr.")
+nyc_pigeon_organizer(pigeon_data)
+p get_pigeon_info(pigeon_data, "Peter Jr.")
 
